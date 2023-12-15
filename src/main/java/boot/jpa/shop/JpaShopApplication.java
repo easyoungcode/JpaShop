e57e56182ad02cf828387c5bcc9f4696d68f5d73
ez0 <101056943+easyoungcode@users.noreply.github.com>
2023-12-15 13:43:25 +0900

    [INIT] project init

---
+++ src/main/java/boot/jpa/shop/JpaShopApplication.java
@@ -0,0 +1,13 @@
+package boot.jpa.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/boot/jpa/shop/JpaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.jpa.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
